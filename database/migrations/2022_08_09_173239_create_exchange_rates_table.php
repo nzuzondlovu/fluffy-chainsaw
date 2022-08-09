9c0f165cf0f91fe3330f442707d2f2e8776e2285
Nzuzo Ndlovu <nzuzo@hellofirstview.com>
2022-08-09 19:42:23 +0200

    Added migration files as well as controllers and model classes for symbols and exchange rates

---
+++ database/migrations/2022_08_09_173239_create_exchange_rates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchange_rates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('symbol_id')->unsigned()->index();
+            $table->foreign('symbol_id')->references('id')->on('symbols')->onDelete('cascade');
+            $table->string('symbol_code');
+            $table->double('rate_value', 8, 6);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exchange_rates');
+    }
+};
