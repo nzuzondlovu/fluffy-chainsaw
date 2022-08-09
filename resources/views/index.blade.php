9c065ec081430bc8fcc48580b41eea6a2c9d8cbb
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2022-08-09 23:12:07 +0200

    Added error message display logic to help users know what mistakes they made while submitting form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,27 @@
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
             <h1 class="display-4 fw-bolder">Query The Forex Exchange Rates</h1>
-            <p class="lead fw-normal text-white-50 mb-0">Make sure that all fields are field and the dates are correct.</p>
+            <p class="lead fw-normal text-white-50 mb-0">Make sure that all fields are field and the dates are correct.
+            </p>
         </div>
     </div>
 </header>
 @endsection
 
 @section('content')
+@if(Session::get('errors'))
+<h5>There were errors:</h5>
+@foreach($errors->all() as $message)
+<div class="alert alert-danger d-flex align-items-center" role="alert">
+    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+        <use xlink:href="#exclamation-triangle-fill" />
+    </svg>
+    <div>
+        {{$message}}
+    </div>
+</div>
+@endforeach
+@endif
 <div class="row">
     <div class="col mb-5">
         <form method="POST" action="/search">
@@ -22,7 +36,7 @@
                 <label for="base_symbol">Base Currency</label>
                 <select name="base_symbol" class="form-control" id="base_symbol" required>
                     @foreach ($symbols as $symbol)
-                    <option value="{{ $symbol->code }}">{{ $symbol->code }}</option>
+                    <option value="{{ $symbol->code }}">{{ $symbol->code }} - {{ $symbol->name }}</option>
                     @endforeach
                 </select>
             </div>
