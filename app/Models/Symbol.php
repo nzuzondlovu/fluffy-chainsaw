6dbfe9b5cbeb44ce490ef6a18c5243995efd51cf
Nzuzo Ndlovu <nzuzondlovu@gmail.com>
2022-08-09 19:50:45 +0200

    Updated the model class to add mass fillable array as well as the relationship with the exchange rate class

--- app/Models/Symbol.php
+++ app/Models/Symbol.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Symbol extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'code',
+    ];
+
+    /**
+     * Get the exchange rates that belongs to the symbol.
+     */
+    public function exchange_rates()
+    {
+        return $this->hasMany(ExchangeRate::class);
+    }
 }
